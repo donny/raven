bf59367319f183d609aeac51fff0eff4428d85ce
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-05-01 15:52:02 +1000

    Add initial files from Mix

---
+++ lib/raven.ex
@@ -0,0 +1,18 @@
+defmodule Raven do
+  @moduledoc """
+  Documentation for Raven.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Raven.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/raven_test.exs
@@ -0,0 +1,8 @@
+defmodule RavenTest do
+  use ExUnit.Case
+  doctest Raven
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
